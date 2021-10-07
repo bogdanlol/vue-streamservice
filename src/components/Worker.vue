cadf4652063b11d94b7025bfba4e0c70f844f852
Bogdanlol <luca.bogdan95@gmail.com>
2021-10-07 12:23:02 +0300

    chore(worker): redesign

--- src/components/Worker.vue
+++ src/components/Worker.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app id="inspire">
 
-    <v-main class="grey lighten-4">
+    <v-main class="grey lighten-5">
       <v-container>
         <v-row>
           <v-col
+          
             cols="12"
             sm="3"
           >
@@ -12,11 +13,12 @@
               rounded="lg"
               min-height="73vh"
             >
-            <v-card class="dark-orange" dark min-height="78vh">
+            <v-card   min-height="78vh">
               <div>
-                &nbsp;
-                <h4 class="text-center">Worker Information</h4>
-                <v-card-title class="justify-center"><v-icon x-large>mdi-information-outline</v-icon></v-card-title>
+                <v-toolbar color="orange darken-4">
+                <h4 class="text-center">Worker Information <v-icon x-large>mdi-information-outline</v-icon></h4>
+                </v-toolbar>
+                <v-card-title class="justify-center"></v-card-title>
                 <div class="send">
                 <div class ="row">
                   <div class="col">Worker name </div>  <div class="col-3">{{name}}</div> 
@@ -44,10 +46,11 @@
             <v-sheet
               min-height="73vh"
               rounded="lg"
-              color="darken-4"
-              dark
+              
             >
-              <p align="center" style="font-size:20px;"><br>Streaming Status</p>
+              <v-toolbar color="orange darken-4">
+                <v-toolbar-title >Streaming status</v-toolbar-title>
+              </v-toolbar>
               <v-data-table
             :headers="headers"
             :items="connectors"
@@ -66,10 +69,49 @@
             <v-sheet
               rounded="lg"
               min-height="73vh"
-              color="darken-1"
-              dark
+      
+              
             >
-           
+            
+            <v-form ref="form" v-model="isFormValid" lazy-validation>
+        <v-toolbar color="orange darken-4">
+                <v-toolbar-title>Editable Information</v-toolbar-title>
+              </v-toolbar>
+               <v-card-title class="justify-center"></v-card-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="sshKey"
+            label="sshKey"
+            required
+            dense>
+            </v-text-field>
+         
+
+
+            <v-select 
+              :items="environments"
+
+              v-model="environment"
+              v-on:change="changeType()"
+              label="Connector Class"
+              required
+              dense>
+              </v-select>
+      
+     
+
+     
+        <v-text-field
+          v-model="ip"
+          label="Ip"
+          required
+          dense>
+        </v-text-field>
+      
+
+         
+        
+      </v-form>
         
             </v-sheet>
           </v-col>
@@ -86,7 +128,7 @@
               min-height="268"
             >
             <v-card class="mx-auto">
-              <v-toolbar color="black darken-1" dark><v-icon small>mdi-help-circle-outline</v-icon>&nbsp;
+              <v-toolbar color="orange darken-4"><v-icon small>mdi-help-circle-outline</v-icon>&nbsp;
                 <v-toolbar-title>Worker Properties</v-toolbar-title>
               </v-toolbar>
                 <pre>{{workerproperties}}</pre>
@@ -107,7 +149,7 @@
               min-height="268"
             >
             <v-card class="mx-auto">
-              <v-toolbar color="blue-white darken-1" dark><v-icon small>mdi-help-circle-outline</v-icon>&nbsp;
+              <v-toolbar color="orange darken-4" ><v-icon small>mdi-help-circle-outline</v-icon>&nbsp;
                 <v-toolbar-title>Error Log</v-toolbar-title>
               </v-toolbar>
                 <pre>{{errorLog}}</pre>
@@ -128,6 +170,10 @@ export default {
   name: "worker",
   data() {
     return {
+        environments:['TST','DEV','ACC','PRD'],
+        environment:"",
+        sshKey:"",
+        ip:"",
         isFormValid: false,
         id:null,
         name: "",
@@ -169,12 +215,16 @@ export default {
     await WorkerService.getWorker(this.$route.params.id)
         .then((response) => {
   
-         this.name=response.data.data.worker.name;
-         this.path=response.data.data.worker.path;
-         this.port=response.data.data.worker.port;
-         this.workerproperties = response.data.data.WorkerProperties;
-          this.errorLog = response.data.data.errorLog;
-          this.connectors = response.data.data.connectors;
+        this.name=response.data.data.worker.name;
+        this.path=response.data.data.worker.path;
+        this.port=response.data.data.worker.port;
+        this.workerproperties = response.data.data.WorkerProperties;
+        this.errorLog = response.data.data.errorLog;
+        this.connectors = response.data.data.connectors;
+        this.environment =response.data.data.worker.environment;
+        this.sshKey = response.data.data.worker.sshKey;
+        this.ip = response.data.data.worker.ip;
+
          if (response.data.data.connectors && response.data.data.connectors.length !=0){
            this.status = "Connectors are running on this worker"
          }else{
